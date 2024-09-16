787255b9bd7a99fad3e96dca69856c9cb532008f
andrejanev96 <107257302+AndrejJanev@users.noreply.github.com>
2024-09-16 14:42:16 +0200

    added a sign-up message

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const signUpBtn = document.querySelector('#sign-up');
 
 signUpBtn.addEventListener('click', updateBtn);
+signUpBtn.addEventListener('click', greet);
 
 function updateBtn() {
     if (signUpBtn.textContent === 'Sign Up') {
         signUpBtn.textContent = "You're awesome for signing up!"
         signUpBtn.disabled= true;
     }
+}
+
+function greet() {
+    const name = prompt("What's your name?");
+    const greeting = document.querySelector("#greeting");
+    greeting.textContent = `Hello ${name}, thank you for subscribing to our website!`
 }
